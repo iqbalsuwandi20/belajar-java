cf73b38b7a9ce08e7e5ad4cdb6d139c87dab0668
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2023-04-06 17:54:13 +0700

    add tipedatanumber.java

---
+++ TipeDataNumber.java
@@ -0,0 +1,23 @@
+public class TipeDataNumber {
+    public static void main(String[] args) {
+        // Integer
+        byte iniByte = 127;
+        short iniShort = 1000;
+        int iniInt = 1000000000;
+        long iniLong = 100000000;
+        long iniLong2 = 100000000000000000L;
+
+        // Floating Point
+        float iniFloat = 10.10F;
+        double iniDouble = 10.10;
+
+        // Literals
+        int decimalInt = 25;
+        int hexaInt = 0xA132B;
+        int binaryInt = 0b010101;
+
+        // Underscore
+        long balance = 1_000_000_000L;
+        int sum = 60_000_000;
+    }
+}
