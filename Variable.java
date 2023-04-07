eb69124689df2f951014a080c2115ba5eb01f3e9
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2023-04-08 03:34:40 +0700

    add variable.java

---
+++ Variable.java
@@ -0,0 +1,17 @@
+public class Variable {
+    public static void main(String[] args) {
+        String name;
+        name = "Iqbal Suwandi";
+
+        System.out.println(name);
+
+        int age = 22;
+        String address = "Indonesia";
+
+        System.out.println(age);
+        System.out.println(address);
+
+        name = "Steven";
+        System.out.println(name);
+    }
+}
