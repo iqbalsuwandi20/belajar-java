0f7da5c804bd558fe7ffad3e3c778e5f487a82d3
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2023-04-08 03:43:44 +0700

    add tipedatabukanprimitif.java

---
+++ TipeDataBukanPrimitif.java
@@ -0,0 +1,18 @@
+import javax.annotation.processing.SupportedOptions;
+
+public class TipeDataBukanPrimitif {
+    public static void main(String[] args) {
+        Integer iniInteger = 100;
+        Long iniLong = 100000L;
+
+        Byte iniByte = null;
+        System.out.println(iniByte);
+
+        iniByte = 100;
+        System.out.println(iniByte);
+
+        //Konversi tipe primitif ke tipe bukan primitif
+        int iniInt = 100;
+        Integer integer2 = iniInt;
+    }
+}
