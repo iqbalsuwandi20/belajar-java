37185045d4cde19f3d415675fd8318d37f25628e
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2023-04-07 14:06:09 +0700

    add tipedatastring.java

---
+++ TipeDataString.java
@@ -0,0 +1,14 @@
+public class TipeDataString {
+    public static void main(String[] args) {
+        String firstName = "Iqbal";
+        String lastName = "Suwandi";
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+
+        // Menggabungkan String
+        String fullName = firstName + " " + lastName;
+
+        System.out.println(fullName);
+    }
+}
