f469426beaf1e61e1cfcc55a2309ea6fd1f811a9
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2023-04-07 13:53:50 +0700

    add tipedatachar.java

---
+++ TipeDataChar.java
@@ -0,0 +1,21 @@
+public class TipeDataChar {
+    public static void main(String[] args) {
+        char i = 'I';
+        char q = 'Q';
+        char b = 'B';
+        char a = 'A';
+        char l = 'L';
+
+        System.out.println(i);
+        System.out.println(q);
+        System.out.println(b);
+        System.out.println(a);
+        System.out.println(l);
+
+        System.out.print(i);
+        System.out.print(q);
+        System.out.print(b);
+        System.out.print(a);
+        System.out.print(l);
+    }
+}
