5bd9cac64bd2c10e4b1bb0af7ada6fe8cee37484
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2023-04-08 16:47:05 +0700

    add expressionstatementblock.java

---
+++ ExpressionStatementBlock.java
@@ -0,0 +1,16 @@
+import javax.annotation.processing.SupportedOptions;
+
+public class ExpressionStatementBlock {
+    public static void main(String[] args) {
+        
+      // Statement
+      System.out.println("Hello World 1");
+      System.out.println("Hello World 2");
+      System.out.println("Hello World 3");  
+
+      // Block
+      {
+        System.out.println("Hello World 4");
+      }
+    }
+}
