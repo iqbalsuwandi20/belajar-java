54252a8e350b67a360699866a905887eac885f0b
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2023-04-10 11:30:44 +0700

    add ternaryoperator.java

---
+++ TernaryOperator.java
@@ -0,0 +1,7 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+        var nilai = 75;
+        String ucapan = nilai >= 75 ? "Selamat Anda lulus" : "Silahkan Coba Lagi";
+        System.out.println(ucapan);
+    }
+}
