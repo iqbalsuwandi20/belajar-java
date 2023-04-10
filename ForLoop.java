8d11d71a5a3bae471fa761479d16f789fe8f3dae
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2023-04-10 11:40:59 +0700

    add forloop.java

---
+++ ForLoop.java
@@ -0,0 +1,19 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        // Perulangan Tanpa Henti
+        // for(;;) {
+        //     System.out.println("Perulangan Tanpa Henti");
+        // }
+
+        // Perulangan Dengan Init Statement
+        for (var counter = 1; counter <= 10;) {
+            System.out.println("Perulangan " + counter);
+            counter++;
+        }
+        
+        // Perulangan Dengan Post Statement
+        for (var counter = 1; counter <= 10; counter++) {
+            System.out.println("Perulangan " + counter);
+        }
+    }
+}
