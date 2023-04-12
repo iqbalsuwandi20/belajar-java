9fe63ceb3187a86202b22c39ad809ad87bd4b08f
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2023-04-12 17:48:07 +0700

    cara membuat dan mengakses method di java

--- Person.java
+++ Person.java
@@ -2,4 +2,9 @@ class Person {
     String name;
     String address;
     final String country = "Indonesia";
+
+    // Cara membuat method
+    void sayHello(String paramName) {
+        System.out.println("Hello " + paramName + ", My Name is " + name);
+    }
 }
--- PersonApp.java
+++ PersonApp.java
@@ -1,12 +1,20 @@
 public class PersonApp {
     public static void main(String[] args) {
+        Person person2 = new Person();
+        Person person3;
+        person3 = new Person();
+        person3.name = "Jakol";
+
         var person1 = new Person();
         person1.name = "Iqbal";
         person1.address = "Tangerang";
         // person1.country = "Belanda"; ERROR KARENA FINAL VAR
-        
-        Person person2 = new Person();
-        Person person3;
-        person3 = new Person();
+        System.out.println(person1.name);
+        System.out.println(person1.address);
+        System.out.println(person1.country);
+
+        // Cara mengakses method
+        person1.sayHello("Steven");
+        person3.sayHello("Garul");
     }
 }
