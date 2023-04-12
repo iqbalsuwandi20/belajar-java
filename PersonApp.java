e46b9f91d6bae5046a513bdbde338c32819190a9
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2023-04-12 17:39:41 +0700

    cara membuat field di java

--- PersonApp.java
+++ PersonApp.java
@@ -1,12 +1,12 @@
 public class PersonApp {
     public static void main(String[] args) {
         var person1 = new Person();
+        person1.name = "Iqbal";
+        person1.address = "Tangerang";
+        // person1.country = "Belanda"; ERROR KARENA FINAL VAR
+        
         Person person2 = new Person();
         Person person3;
         person3 = new Person();
-
-        System.out.println(person1);
-        System.out.println(person2);
-        System.out.println(person3);
     }
 }
