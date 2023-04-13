6b936b9621076cb183de8efe7fe85ff9bb6f2b18
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2023-04-14 03:47:08 +0700

    cara membuat package di java

---
+++ belajarjavapack/data/Product.java
@@ -0,0 +1,12 @@
+package belajarjavapack.data;
+
+// Cara membuat package
+class Product {
+    String name;
+    int price;
+
+    Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    } 
+}
