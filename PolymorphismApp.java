419b00500307c028b8046856dd1c31e6a8e5d98f
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2023-04-13 20:08:26 +0700

    cara membuat type check dan casts di java

--- PolymorphismApp.java
+++ PolymorphismApp.java
@@ -18,6 +18,15 @@ public class PolymorphismApp {
 
     // Cara mengakses method polymorphism
     static void sayHello(Employee employee) {
-        System.out.println("Hello " + employee.name);
+        // Cara membuat type check dan casts
+        if(employee instanceof VicePresident) {
+            VicePresident vicePresident = (VicePresident) employee;
+            System.out.println("Hello VP " + vicePresident.name);
+        } else if(employee instanceof Manager) {
+            Manager manager = (Manager) employee;
+            System.out.println("Hello Manager " + manager.name);
+        } else {
+            System.out.println("Hello " + employee.name);
+        }
     }
 }
