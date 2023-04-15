d2ff9d2587c649aabf56386fe88afe1463b20054
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2023-04-15 17:40:59 +0700

    cara membuat final class di java

---
+++ belajarjavapack/data/SocialMedia.java
@@ -0,0 +1,12 @@
+package belajarjavapack.data;
+
+// Cara membuat final class 
+public class SocialMedia {
+    String name;   
+}
+
+final class Facebook extends SocialMedia {
+
+}
+
+// class FakeFacebook extends Facebook {} ERROR
