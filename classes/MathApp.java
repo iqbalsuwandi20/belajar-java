abc070d5dab6351a310f9e0854b66f6bb4a34f36
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2023-04-21 00:02:10 +0700

    cara membuat math class

---
+++ classes/MathApp.java
@@ -0,0 +1,17 @@
+package classes;
+
+/**
+ * Membuat Math Class
+ */
+public class MathApp {
+    public static void main(String[] args) {
+        
+        var min = Math.min(3000, 4121);
+        System.out.println(min);
+
+        var max = Math.max(3000, 4121);
+        System.out.println(max);
+
+        System.out.println(Math.PI);
+    }
+}
