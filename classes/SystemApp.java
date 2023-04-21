d79f8dc6e9b22f85ac31f937f358ef2ae9b704a8
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2023-04-21 14:47:20 +0700

    cara membuat system class

---
+++ classes/SystemApp.java
@@ -0,0 +1,18 @@
+package classes;
+
+/**
+ * Membuat System Class
+ */
+public class SystemApp {
+    public static void main(String[] args) {
+        
+        System.out.println(System.currentTimeMillis());
+        System.out.println(System.nanoTime());
+
+        System.out.println(System.getenv("HOME"));
+
+        System.gc();
+
+        System.exit(0);
+    }
+}
