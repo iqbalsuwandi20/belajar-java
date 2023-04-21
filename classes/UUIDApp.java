607231c95dcbc98fc3750d691204d008d90b797e
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2023-04-21 14:56:07 +0700

    cara membuat uuid class

---
+++ classes/UUIDApp.java
@@ -0,0 +1,18 @@
+package classes;
+
+import java.util.UUID;
+
+/**
+ * Membuat UUID Class
+ */
+public class UUIDApp {
+    public static void main(String[] args) {
+        
+        for (var i = 0; i < 100; i++) {
+            UUID uuid = UUID.randomUUID();
+            String key = uuid.toString();
+
+            System.out.println(key);
+        }
+    }
+}
