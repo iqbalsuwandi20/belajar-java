69a2fc6ed1f3acdda17c45f2c453c9df9ac33f50
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2023-04-25 13:39:09 +0700

    cara membuat List

---
+++ collection/ListApp.java
@@ -0,0 +1,22 @@
+// package collection;
+
+// import java.util.List;
+
+// /**
+//  * List
+//  */
+// public class ListApp {
+//     public static void main(String[] args) {
+        
+//         List<String> Strings = new ArrayList<>();
+
+//         Strings.add("Steven");
+//         Strings.add("Suwandi");
+
+//         Strings.set(0, "Iqbal");
+
+//         Strings.remove(1);
+
+//         System.out.println(Strings.get(0));
+//     }
+// }
