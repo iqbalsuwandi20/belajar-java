f81fd1494a9190550f49c73a389e85202c18ebfa
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2023-04-27 09:06:45 +0700

    cara membuat SortedSet

---
+++ collection/data/PersonComparator.java
@@ -0,0 +1,15 @@
+// package data;
+
+// import java.util.Comparator;
+
+// /**
+//  * SortedSet
+//  */
+// public class PersonComparator implements Comparator<Person> {
+
+//     @Override
+//     public int compare(Person o1, Person o2) {
+//         return o1.getName().compareTo(o2.getName());
+//     }
+    
+// }
